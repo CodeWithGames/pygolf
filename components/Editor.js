4206df4396b31c6777d72772ebc513da5871cd53
Cooper Saye <coopersaye@gmail.com>
2021-07-20 18:22:15 -0700

    Implement code running.

--- components/Editor.js
+++ components/Editor.js
@@ -8,12 +8,23 @@ import styles from '../styles/Editor.module.css';
 export default function Editor() {
   const [code, setCode] = useState('');
 
+  // runs python code at shell endpoint
+  async function run() {
+    const res = await fetch(`/api/shell?code=${encodeURIComponent(code)}`);
+    const data = await res.json();
+    if (data.error) console.error(data.error.traceback ?? 'unknown error');
+    else console.log(data.result.join('\n'));
+  }
+
   return (
-    <AceEditor
-      value={code}
-      onChange={val => setCode(val)}
-      mode="python"
-      theme="monokai"
-    />
+    <div>
+      <AceEditor
+        value={code}
+        onChange={val => setCode(val)}
+        mode="python"
+        theme="monokai"
+      />
+      <button onClick={run}>Run</button>
+    </div>
   );
 }
