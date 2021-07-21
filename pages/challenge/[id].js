b7364dcf10164199d5c77b48e132025c3a51774b
Cooper Saye <coopersaye@gmail.com>
2021-07-21 13:16:10 -0700

    Create challenge page.

---
+++ pages/challenge/[id].js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+
+import firebase from 'firebase/app';
+
+import dynamic from 'next/dynamic';
+const Editor = dynamic(import('../../components/Editor.js'), { ssr: false });
+
+export default function Challenge() {
+  const [challengeData, setChallengeData] = useState(undefined);
+
+  const router = useRouter();
+  const { id } = router.query;
+
+  // retrieves challenge data from firebase
+  async function getChallengeData() {
+    const challengesRef = firebase.firestore().collection('challenges');
+    const challengeDoc = await challengesRef.doc(id).get();
+    if (challengeDoc.exists) setChallengeData(challengeDoc.data());
+    else router.push('/challenges');
+  }
+
+  // retrieve challenge data on start
+  useEffect(() => {
+    if (id) getChallengeData();
+  }, [id]);
+
+  if (challengeData === undefined) return <div>Loading...</div>;
+  if (challengeData === null) return <div>Challenge not found</div>;
+
+  return <Editor data={challengeData} />;
+}
