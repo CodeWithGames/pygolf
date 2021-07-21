0fc224c54e9f68de3f24a5a3721437c2c47b80e7
Cooper Saye <coopersaye@gmail.com>
2021-07-21 11:21:03 -0700

    Set up landing page.

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,26 @@
-import dynamic from 'next/dynamic';
-const Editor = dynamic(import('../components/Editor.js'), { ssr: false });
+import Link from 'next/link';
+
+import firebase from 'firebase/app';
 
 import styles from '../styles/Index.module.css';
 
 export default function Index() {
+  // opens google sign in popup
+  function signInWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  }
+
   return (
     <div>
-      <Editor />
+      <h1>Pygolf</h1>
+      {
+        firebase.auth().currentUser ?
+        <Link href="/challenges">
+          <a>Challenges</a>
+        </Link> :
+        <button onClick={signInWithGoogle}>Sign in with Google</button>
+      }
     </div>
   );
 }
