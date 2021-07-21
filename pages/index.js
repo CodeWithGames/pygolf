6a9448285c44284596198aadca9b284999a57f6b
Cooper Saye <coopersaye@gmail.com>
2021-07-21 11:47:37 -0700

    Create user docs.

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,30 @@ import firebase from 'firebase/app';
 import styles from '../styles/Index.module.css';
 
 export default function Index() {
+  // creates user doc if none exists
+  async function createUserDoc() {
+    // retrieve user doc
+    const currentUser = firebase.auth().currentUser;
+    const uid = currentUser.uid;
+    const usersRef = firebase.firestore().collection('users');
+    const userRef = usersRef.doc(uid);
+    const userDoc = await userRef.get();
+    // if no user doc exists
+    if (!userDoc.exists) {
+      // create user doc
+      userRef.set({
+        username: uid.slice(0, 16),
+        picture: currentUser.photoURL,
+        joined: new Date()
+      });
+    }
+  }
+
   // opens google sign in popup
-  function signInWithGoogle() {
+  async function signInWithGoogle() {
     const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider);
+    await firebase.auth().signInWithPopup(provider);
+    await createUserDoc();
   }
 
   return (
