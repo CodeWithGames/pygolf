50fcb83dea7ae54167bb73cc99b5661ccd7262f7
Cooper Saye <coopersaye@gmail.com>
2021-07-20 18:47:23 -0700

    Display program output.

--- components/Editor.js
+++ components/Editor.js
@@ -7,24 +7,53 @@ import styles from '../styles/Editor.module.css';
 
 export default function Editor() {
   const [code, setCode] = useState('');
+  const [output, setOutput] = useState({ error: false, text: '' });
 
   // runs python code at shell endpoint
   async function run() {
+    // reset output
+    setOutput({ error: false, text: '' });
+    // request endpoint
     const res = await fetch(`/api/shell?code=${encodeURIComponent(code)}`);
     const data = await res.json();
-    if (data.error) console.error(data.error.traceback ?? 'unknown error');
-    else console.log(data.result.join('\n'));
+    // set output based on response
+    if (data.error) setOutput({
+      error: true,
+      text: data.error.traceback ?? 'unknown error'
+    });
+    else setOutput({
+      error: false,
+      text: data.result ? data.result.join('\n') : ''
+    });
   }
 
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.toolbar}>
+        <p>{code.length}</p>
+        <button onClick={run}>Run</button>
+      </div>
       <AceEditor
         value={code}
         onChange={val => setCode(val)}
         mode="python"
         theme="monokai"
+        wrapEnabled={true}
+        showPrintMargin={false}
+        width="100%"
       />
-      <button onClick={run}>Run</button>
+      <div className={styles.console}>
+        <div className={styles.output}>
+          {
+            output.error ?
+            <p className={styles.errortext}>{output.text}</p> :
+            <p>{output.text}</p>
+          }
+        </div>
+        <div className={styles.output}>
+
+        </div>
+      </div>
     </div>
   );
 }
