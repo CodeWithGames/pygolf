9eba2a8ee38239def32d9c6331053af85b1c2741
Cooper Saye <coopersaye@gmail.com>
2021-07-21 20:36:37 -0700

    Improve styling.

--- components/Header.js
+++ components/Header.js
@@ -1,20 +1,26 @@
+import Link from 'next/link';
+
 import firebase from 'firebase/app';
 
 import styles from '../styles/Header.module.css';
 
 export default function Header() {
-  // opens google sign in popup
-  function signInWithGoogle() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider);
-  }
-
   return (
     <div className={styles.container}>
+      <Link href="/challenges">
+        <a>Challenges</a>
+      </Link>
       {
         firebase.auth().currentUser ?
-        <button onClick={() => firebase.auth().signOut()}>Sign Out</button> :
-        <button onClick={signInWithGoogle}>Sign in with Google</button>
+        <>
+          <Link href="/create">
+            <a>Create</a>
+          </Link>
+          <button onClick={() => firebase.auth().signOut()}>Sign Out</button>
+        </> :
+        <Link href="/">
+          <a>Home</a>
+        </Link>
       }
     </div>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -24,7 +24,7 @@ function App({ Component, pageProps }) {
         <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png" />
-        <link rel="manifest" href="manifest.json" />
+        <link rel="manifest" href="/manifest.json" />
       </Head>
       <Header />
       <Component {...pageProps} />
