5f7f73ce30e6fb05a796bee5e41bc9cecd6ffd1f
Cooper Saye <coopersaye@gmail.com>
2021-07-25 21:12:52 -0700

    Improve landing page styling.

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,33 @@
 import Link from 'next/link';
-import signInWithGoogle from '../util/signInWithGoogle.js';
+import Image from 'next/image';
 
+import signInWithGoogle from '../util/signInWithGoogle.js';
 import firebase from 'firebase/app';
 
 import styles from '../styles/Index.module.css';
 
 export default function Index() {
   return (
-    <div>
-      <h1>Pygolf</h1>
-      {
-        !firebase.auth().currentUser &&
-        <button onClick={signInWithGoogle}>Sign in with Google</button>
-      }
+    <div className={styles.container}>
+      <div className={styles.box}>
+        <div className={styles.title}>
+          <Image
+            src="/img/logo.png"
+            width="40px"
+            height="40px"
+          />
+          <h1>Pygolf</h1>
+        </div>
+        {
+          firebase.auth().currentUser ?
+          <Link href="/challenges">
+            <a className="url">Challenges</a>
+          </Link> :
+          <button onClick={signInWithGoogle} className="btn btn-primary">
+            Sign in with Google
+          </button>
+        }
+      </div>
     </div>
   );
 }
