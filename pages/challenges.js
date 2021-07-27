91c0a77dda0245a5a547b748af23a99e9ebec5f0
Cooper Saye <coopersaye@gmail.com>
2021-07-26 21:51:03 -0700

    Implement challenge tabs.

--- pages/challenges.js
+++ pages/challenges.js
@@ -1,13 +1,19 @@
 import Link from 'next/link';
 import Loading from '../components/Loading.js';
 import Challenge from '../components/Challenge.js';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
+import { useState } from 'react';
 import firebase from 'firebase/app';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 import styles from '../styles/Challenges.module.css';
 
 export default function Challenges(props) {
+  const [tab, setTab] = useState(0);
+
+    // retrieve all challenges
   const challengesRef = firebase.firestore().collection('challenges');
   const [challenges] = useCollectionData(
     challengesRef.orderBy('created', 'desc'), { idField: 'id' }
@@ -17,15 +23,11 @@ export default function Challenges(props) {
 
   return (
     <div className={styles.container}>
-      {
-        challenges.map(challenge =>
-          <Challenge
-            key={challenge.id}
-            data={challenge}
-            userData={props.userData}
-          />
-        )
-      }
+      <Tabs value={tab} onChange={(e, val) => setTab(val)}>
+        <Tab label="All Challenges" />
+        {firebase.auth().currentUser && <Tab label="Your Challenges" />}
+        {firebase.auth().currentUser && <Tab label="Starred Challenges" />}
+      </Tabs>
     </div>
   );
 }
