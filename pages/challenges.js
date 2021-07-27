2f4afb2fe44f7511e8e34c5c9ee16503b413db97
Cooper Saye <coopersaye@gmail.com>
2021-07-26 21:51:55 -0700

    Show personal and starred challenges.

--- pages/challenges.js
+++ pages/challenges.js
@@ -13,13 +13,22 @@ import styles from '../styles/Challenges.module.css';
 export default function Challenges(props) {
   const [tab, setTab] = useState(0);
 
-    // retrieve all challenges
+  // retrieve all challenges
   const challengesRef = firebase.firestore().collection('challenges');
   const [challenges] = useCollectionData(
     challengesRef.orderBy('created', 'desc'), { idField: 'id' }
   );
-
-  if (!challenges) return <Loading />;
+  // retrieve user challenges
+  const uid = firebase.auth().currentUser?.uid;
+  const [userChallenges] = useCollectionData(
+    challengesRef.where('creator', '==', uid ?? 'null')
+    .orderBy('created', 'desc'), { idField: 'id' }
+  );
+  // retrieve starred challenges
+  const [starredChallenges] = useCollectionData(
+    challengesRef.where('stars', 'array-contains', uid ?? 'null')
+    .orderBy('created', 'desc'), { idField: 'id' }
+  );
 
   return (
     <div className={styles.container}>
@@ -28,6 +37,26 @@ export default function Challenges(props) {
         {firebase.auth().currentUser && <Tab label="Your Challenges" />}
         {firebase.auth().currentUser && <Tab label="Starred Challenges" />}
       </Tabs>
+      <div className={styles.challengelist}>
+        {
+          (tab === 0 && challenges) &&
+          challenges.map(challenge =>
+            <Challenge data={challenge} key={challenge.id} />
+          )
+        }
+        {
+          (tab === 1 && userChallenges) &&
+          userChallenges.map(challenge =>
+            <Challenge data={challenge} key={challenge.id} />
+          )
+        }
+        {
+          (tab === 2 && starredChallenges) &&
+          starredChallenges.map(challenge =>
+            <Challenge data={challenge} key={challenge.id} />
+          )
+        }
+      </div>
     </div>
   );
 }
