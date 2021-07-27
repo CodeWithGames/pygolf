fe65972d286eb0797301659be9da4ccabff9bacf
Cooper Saye <coopersaye@gmail.com>
2021-07-26 23:23:22 -0700

    Retrieve solution creator username.

--- components/Solution.js
+++ components/Solution.js
@@ -1,14 +1,24 @@
 import Link from 'next/link';
 
+import { useEffect, useState } from 'react';
+import getUsername from '../util/getUsername.js';
+
 import styles from '../styles/Solution.module.css';
 
 export default function Solution(props) {
   const { id, length } = props.data;
 
+  const [username, setUsername] = useState('');
+
+  // get username on start
+  useEffect(() => {
+    getUsername(id).then(u => setUsername(u));
+  }, []);
+
   return (
     <div className={styles.container}>
       <Link href={`/user/${id}`}>
-        <a className="url">@{id}</a>
+        <a className="url">@{username}</a>
       </Link>
       <span>•</span>
       <p>{length} characters</p>
