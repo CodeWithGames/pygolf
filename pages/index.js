d7a0b3440f9ebd0e7eb7336f4debac7b410dcc2b
Cooper Saye <coopersaye@gmail.com>
2021-11-13 11:35:07 -0800

    Move about content to index page.

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,40 @@ export default function Index() {
           />
           <h1>Pygolf</h1>
         </div>
+        <p>Create and share Python code golf challenges.</p>
+        <h2>What is code golfing?</h2>
+        <p>Code golfing is a competition in which a programming challenge is
+        solved in the fewest number of characters.</p>
+        <Image
+          src="/img/undraw/golf.svg"
+          width="300"
+          height="300"
+        />
+        <h2>Why Python?</h2>
+        <p>Python syntax is generally quite concise and intuitive, making it
+        an ideal candidate for recreational code golfing.</p>
+        <h2>Can I create challenges?</h2>
+        <p>
+          Yes, anyone with an account can create challenges at{' '}
+          <Link href="/create">
+            <a className="url">/create</a>
+          </Link>
+          .
+        </p>
+        <h2>Can I contribute to the site?</h2>
+        <p>
+          Yes, Pygolf is open source. You can find our GitHub repository here:
+          {' '}
+          <a
+            className="url"
+            href="https://github.com/csaye/pygolf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            csaye/pygolf
+          </a>
+          .
+        </p>
         {
           firebase.auth().currentUser ?
           <Link href="/challenges">
